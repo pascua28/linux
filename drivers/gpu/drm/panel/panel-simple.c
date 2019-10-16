56701714b40fa78a9310fd4b8c4aa7709cdd61a6
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-10-16 16:49:45 +0800

    drm/panel: simple: add sm,s6e3fa2 panel

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -2790,6 +2790,34 @@ static const struct panel_desc_dsi panasonic_vvx10f004b00 = {
 	.lanes = 4,
 };
 
+static const struct drm_display_mode s6e3fa2_mode = {
+	.clock = 149769,
+	.hdisplay = 1080,
+	.hsync_start = 1080 + 162,
+	.hsync_end = 1080 + 162 + 10,
+	.htotal = 1080 + 162 + 10 + 36,
+	.vdisplay = 1920,
+	.vsync_start = 1920 + 13,
+	.vsync_end = 1920 + 13 + 2,
+	.vtotal = 1920 + 13 + 2 + 3,
+	.vrefresh = 60,
+};
+
+static const struct panel_desc_dsi sm_s6e3fa2  = {
+	.desc = {
+		.modes = &s6e3fa2_mode,
+		.num_modes = 1,
+		.bpc = 8,
+		.size = {
+			.width = 65,
+			.height = 115,
+		},
+	},
+	.flags = 0,
+	.format = MIPI_DSI_FMT_RGB888,
+	.lanes = 4,
+};
+
 static const struct of_device_id dsi_of_match[] = {
 	{
 		.compatible = "auo,b080uan01",
@@ -2806,6 +2834,9 @@ static const struct of_device_id dsi_of_match[] = {
 	}, {
 		.compatible = "panasonic,vvx10f004b00",
 		.data = &panasonic_vvx10f004b00
+	}, {
+		.compatible = "sm,s6e3fa2",
+		.data = &sm_s6e3fa2
 	}, {
 		/* sentinel */
 	}
